b8774e4176f7c5e28222149f6730649738effeba
Saul Budie <101558088+SaulBudie@users.noreply.github.com>
2022-04-06 08:18:52 +0700

    Update drawshit.py

--- drawshit.py
+++ drawshit.py
@@ -1,7 +1,8 @@
 num = eval(input("Number pls?: \n"))
 #expand first
 for seggs in range(num):
-    print(" " * (num - seggs), "*" * (2*seggs + 1))
+    print(" " * (num - seggs), "ඞ" * (2*seggs + 1))
 #reduce in the bottom
 for seggs in range(num - 2, -1, -1):
-    print(" " * (num - seggs), "*" * (2*seggs + 1))
+    print(" " * (num - seggs), "ඞ" * (2*seggs + 1))
+#Sussy
